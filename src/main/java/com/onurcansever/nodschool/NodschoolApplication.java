6ca3c69d57ce83f46be54871359d6d5de4aa2506
Onurcan Sever <onsever@icloud.com>
2023-08-03 00:54:10 -0400

    project bootstrap

---
+++ src/main/java/com/onurcansever/nodschool/NodschoolApplication.java
@@ -0,0 +1,13 @@
+package com.onurcansever.nodschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NodschoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NodschoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onurcansever/nodschool/NodschoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onurcansever.nodschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NodschoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
