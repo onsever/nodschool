68c3d7f39b18a392e8652750b7c13661bcdc3f85
Onurcan Sever <onsever@icloud.com>
2023-08-04 02:20:56 -0400

    refactor code using lombok annotations

--- src/main/java/com/onurcansever/nodschool/model/Holiday.java
+++ src/main/java/com/onurcansever/nodschool/model/Holiday.java
@@ -1,5 +1,8 @@
 package com.onurcansever.nodschool.model;
 
+import lombok.Data;
+
+@Data
 public class Holiday {
 
     private final String day;
@@ -11,21 +14,4 @@ public class Holiday {
         FEDERAL
     }
 
-    public Holiday(String day, String reason, Type type) {
-        this.day = day;
-        this.reason = reason;
-        this.type = type;
-    }
-
-    public String getDay() {
-        return day;
-    }
-
-    public String getReason() {
-        return reason;
-    }
-
-    public Type getType() {
-        return type;
-    }
 }
--- src/main/java/com/onurcansever/nodschool/service/ContactService.java
+++ src/main/java/com/onurcansever/nodschool/service/ContactService.java
@@ -1,8 +1,10 @@
 package com.onurcansever.nodschool.service;
 
 import com.onurcansever.nodschool.model.Contact;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class ContactService {
 
@@ -12,7 +14,7 @@ public class ContactService {
      * @return boolean
      */
     public boolean saveContactForm(Contact contact) {
-        System.out.println(contact);
+        log.info(contact.toString());
         return true;
     }
 }
