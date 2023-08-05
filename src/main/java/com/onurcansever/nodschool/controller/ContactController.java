46c984505dab4001e6bf2419aa8ea632a515a0e3
Onurcan Sever <onsever@icloud.com>
2023-08-05 14:37:47 -0400

    implement bean validations

--- src/main/java/com/onurcansever/nodschool/controller/ContactController.java
+++ src/main/java/com/onurcansever/nodschool/controller/ContactController.java
@@ -2,8 +2,12 @@ package com.onurcansever.nodschool.controller;
 
 import com.onurcansever.nodschool.model.Contact;
 import com.onurcansever.nodschool.service.ContactService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,15 +24,20 @@ public class ContactController {
     }
 
     @RequestMapping("/contact")
-    public String displayContactPage() {
+    public String displayContactPage(Model model) {
+        model.addAttribute("contact", new Contact());
         return "contact.html";
     }
 
     @RequestMapping(value = "/sendContactForm", method = RequestMethod.POST)
-    public ModelAndView sendContactForm(Contact contact) { // @RequestParam
+    public String sendContactForm(@Valid @ModelAttribute("contact") Contact contact, Errors errors) { // @RequestParam
+        if (errors.hasErrors()) {
+            return "contact.html"; // Displaying contact page again along with errors.
+        }
+
         this.contactService.saveContactForm(contact);
 
-        return new ModelAndView("redirect:/contact");
+        return "redirect:/contact"; // Invoking contact action again.
     }
 
 }
