bc3a6703523d761ceaf5ce028af2247994547435
Onurcan Sever <onsever@icloud.com>
2023-08-07 16:21:32 -0400

    refactor for auditing support

--- src/main/java/com/onurcansever/nodschool/NodschoolApplication.java
+++ src/main/java/com/onurcansever/nodschool/NodschoolApplication.java
@@ -3,11 +3,13 @@ package com.onurcansever.nodschool;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories("com.onurcansever.nodschool.repository")
 @EntityScan("com.onurcansever.nodschool.model")
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 public class NodschoolApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/onurcansever/nodschool/utils/Constants.java
+++ src/main/java/com/onurcansever/nodschool/utils/Constants.java
@@ -1,8 +1,6 @@
 package com.onurcansever.nodschool.utils;
 
 public interface Constants {
-
-    public static final String ANONYMOUS = "Anonymous";
     public static final String OPEN = "Open";
     public static final String CLOSE = "Close";
 }
