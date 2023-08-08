0933f87c64b261b017bf2044745846d2c209af57
Onurcan Sever <onsever@icloud.com>
2023-08-07 20:44:10 -0400

    assemble relationship between entities and create repositories, service

--- src/main/java/com/onurcansever/nodschool/controller/RegisterController.java
+++ src/main/java/com/onurcansever/nodschool/controller/RegisterController.java
@@ -1,7 +1,9 @@
 package com.onurcansever.nodschool.controller;
 
 import com.onurcansever.nodschool.model.User;
+import com.onurcansever.nodschool.service.UserService;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -11,6 +13,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class RegisterController {
+
+    private final UserService userService;
+
+    @Autowired
+    public RegisterController(UserService userService) {
+        this.userService = userService;
+    }
 
     @RequestMapping(value = "/register", method = RequestMethod.GET)
     public String displayRegisterPage(Model model) {
@@ -25,6 +34,12 @@ public class RegisterController {
             return "register.html";
         }
 
-        return "redirect:/login?register=true";
+        boolean isSaved = this.userService.createNewUser(user);
+
+        if (isSaved) {
+            return "redirect:/login?register=true";
+        }
+
+        return "register.html";
     }
 }
