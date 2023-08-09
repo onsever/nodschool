1d52b36f5bdc343d12f826ac48c5f87da33fcef9
Onurcan Sever <onsever@icloud.com>
2023-08-09 12:55:04 -0400

    custom authentication provider logic

---
+++ src/main/java/com/onurcansever/nodschool/security/UsernameAndPasswordProvider.java
@@ -0,0 +1,58 @@
+package com.onurcansever.nodschool.security;
+
+import com.onurcansever.nodschool.model.Roles;
+import com.onurcansever.nodschool.model.User;
+import com.onurcansever.nodschool.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class UsernameAndPasswordProvider implements AuthenticationProvider {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UsernameAndPasswordProvider(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String emailAddress = authentication.getName();
+        String password = authentication.getCredentials().toString();
+
+        // Comparing credentials entered by user with the database record.
+        User user = this.userRepository.findByEmail(emailAddress).orElse(null);
+
+        if (user != null && user.getUserId() > 0 && passwordEncoder.matches(password, user.getPassword())) {
+           return new UsernamePasswordAuthenticationToken(user.getName(), null, getGrantedAuthorities(user.getRoles()));
+        } else {
+            throw new BadCredentialsException("Invalid credentials!");
+        }
+    }
+
+    private List<GrantedAuthority> getGrantedAuthorities(Roles roles) {
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+        grantedAuthorities.add(new SimpleGrantedAuthority("ROLE_" + roles.getRoleName()));
+
+        return grantedAuthorities;
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+}
